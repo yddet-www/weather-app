3da3744041b5185a0952d3e56caf34459e478ec3
wwww-teddy <yddet.www@gmail.com>
2024-03-30 15:49:52 -0500

    Ngetes doang

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from Weather_DB import *
 
 def run():
     test = WeatherDB()
-    print(test.list_columns('hourly_forecast'))
-    print(test.list_columns('location'))
-    print(test.get_tables())
+    
+    ## TESTING LIST COLUMN FUNCTION
+    # print(test.list_columns('hourly_forecast'))
+    # print(test.list_columns('location'))
+    
+    ## TESTING CREATING AND DROPPING TABLE FUNCTION
+    # debug = test.create_table("test1")
+    # print(test.get_tables())
+    # test.drop_table("test1")
+    # print(test.get_tables())
+    
+    ## TESTING INSERT FUNCTION
+    data = [('37.7749', '-122.4194', 'CA', 'San Francisco', '94105', '7th District')]
+    
+    print(test.insert("location", data))
     
     test.close()
 
