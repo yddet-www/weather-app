8eabf7e163cd0253bf633815f400e053ed46d019
wwww-teddy <yddet.www@gmail.com>
2024-04-17 00:37:39 -0500

    Added functions to retrieve daily and hourly forecast, began working on retrieving active alerts

--- weather_gov.py
+++ weather_gov.py
@@ -1,6 +1,7 @@
 import requests
+import json
 
-# Given latitude and longitude decimal values, return json response
+# Given latitude and longitude decimal values, return JSON response
 def get_point(lat, lon):
     response = requests.get(f"https://api.weather.gov/points/{lat},{lon}")
     
@@ -29,6 +30,7 @@ def get_county(lat, lon):
     
     return 0
 
+# Return county ID as per NWS
 def get_countyID(lat, lon):
     response = get_point(lat, lon)
     
@@ -46,3 +48,34 @@ def get_city(lat, lon):
         return response["properties"]["relativeLocation"]["properties"]["city"] # returns city
     
     return 0
+
+# return JSON response of daily forecast API request
+def get_dailyForecast(lat, lon):
+    response = get_point(lat, lon)
+    
+    if(response):
+        response = requests.get(response["properties"]["forecast"]).json()
+        return response
+    
+    return 0
+
+# return JSON response of hourly forecast API request
+def get_hourlyForecast(lat, lon):
+    response = get_point(lat, lon)
+    
+    if(response):
+        response = requests.get(response["properties"]["forecastHourly"]).json()
+        return response
+    
+    return 0
+
+# return JSON response of active alerts in the state
+def get_active_alerts(lat, lon):
+    params = {
+        "status" : "actual",
+        "area" : get_state(lat, lon),
+    }
+    
+    response = requests.get(f"https://api.weather.gov/alerts/active", params=params).json()
+    print(json.dumps(response))
+    return 1
