aec3f2765fd9c19dafdb99ac94748fdaf3c08f2a
wwww-teddy <yddet.www@gmail.com>
2024-04-16 23:04:31 -0500

    Basic API calls to get state, city, count, and county ID from given latitude and longitude values

--- weather_gov.py
+++ weather_gov.py
@@ -1,3 +1,48 @@
 import requests
 
-# To be worked on soon
+# Given latitude and longitude decimal values, return json response
+def get_point(lat, lon):
+    response = requests.get(f"https://api.weather.gov/points/{lat},{lon}")
+    
+    if(response.status_code == 200):
+        return response.json()
+    
+    print(f"Error retrieving API request: {response.status_code}") # print response code
+    return 0 # returns 0 if API call fails
+
+# Return state (2 characters)
+def get_state(lat, lon):
+    response = get_point(lat, lon)
+    
+    if(response):
+        return response["properties"]["relativeLocation"]["properties"]["state"] # returns state
+    
+    return 0
+
+# Return county (string)
+def get_county(lat, lon):
+    response = get_point(lat, lon)
+    
+    if(response):
+        response = requests.get(response["properties"]["county"]).json()
+        return response["properties"]["name"]
+    
+    return 0
+
+def get_countyID(lat, lon):
+    response = get_point(lat, lon)
+    
+    if(response):
+        response = requests.get(response["properties"]["county"]).json()
+        return response["properties"]["id"]
+    
+    return 0
+
+# Return city (string)
+def get_city(lat, lon):
+    response = get_point(lat, lon)
+    
+    if(response):
+        return response["properties"]["relativeLocation"]["properties"]["city"] # returns city
+    
+    return 0
