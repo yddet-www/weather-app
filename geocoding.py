291ffbd5e238cac180fd28f23081c5f9c07203ec
wwww-teddy <yddet.www@gmail.com>
2024-04-17 23:24:52 -0500

    Created module to handle API to https://geocode.maps.co/

---
+++ geocoding.py
@@ -0,0 +1,33 @@
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("API_KEY") # get your own key bruh
+
+# return dictionaries of location data from a given address (its kinda like a google search)
+def search_geocode(address):
+    params = {
+        "q" : address,
+        "api_key" : api_key
+    }
+    
+    response = requests.get("https://geocode.maps.co/search", params=params).json()
+    
+    locations = []
+
+    for location in response:
+        loc_dict = {
+            "display_name": location["display_name"],
+            "lat": location["lat"],
+            "lon": location["lon"]
+        }
+    
+    locations.append(loc_dict)
+    
+    return locations
+
+# return tuple of lat and lon from given location dictionary
+def get_latlon(location):
+    return (location["lat"], location["lon"])
