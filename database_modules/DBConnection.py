d1cc7481e1e664d8924ad353665e5877d703adcf
wwww-teddy <yddet.www@gmail.com>
2024-04-18 15:22:40 -0500

    Now handles connection through pooling

--- database_modules/DBConnection.py
+++ database_modules/DBConnection.py
@@ -1,16 +1,18 @@
-import mysql.connector
+import mysql.connector.pooling
 
-_connection = None
+# Create a connection pool
+_connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="weather-gov-pool",
+    pool_size=5,
+    pool_reset_session=True,
+    host="127.0.0.1",
+    user="root",
+    password="admin1234",
+    database="weather-gov",
+    charset="utf8"
+)
 
 def get_connection():
-    global _connection
-    if _connection is None:
-        _connection = mysql.connector.connect(
-            host = "127.0.0.1", 
-            user = "root", 
-            password = "admin1234", 
-            database = "weather-gov",
-            charset= "utf8"
-        )
-        
-    return _connection
+    # Get a connection from the pool
+    connection = _connection_pool.get_connection()
+    return connection
